4851dca548dcda40150e626425a3b6668512015e
Saglara Tserenova <ts.saglara@gmail.com>
2022-06-08 21:20:54 +0300

    clear functions

---
+++ src/js/health_line.js
@@ -0,0 +1,10 @@
+export default function healthLine (player) {
+    if (player.health > 50) {
+        return 'healthy';
+    }
+    if (player.health > 15) {
+        return 'critical';
+    }
+    return 'wounded';
+};
+
