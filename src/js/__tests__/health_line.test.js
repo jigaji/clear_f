a2d0d282c0c9c8ee2d83221dc15827bff2bc9050
Saglara Tserenova <ts.saglara@gmail.com>
2022-06-08 21:30:01 +0300

    lint

--- src/js/__tests__/health_line.test.js
+++ src/js/__tests__/health_line.test.js
@@ -1,6 +1,6 @@
-import healthLine from "../health_line";
+import healthLine from '../health_line';
 
 test('Mage with 90 health', () => {
-    const result = healthLine({name: 'Маг', health: 90});
-    expect(result).toBe('healthy');    
+  const result = healthLine({ name: 'Маг', health: 90 });
+  expect(result).toBe('healthy');
 });
--- src/js/health_line.js
+++ src/js/health_line.js
@@ -1,10 +1,7 @@
-export default function healthLine (player) {
-    if (player.health > 50) {
-        return 'healthy';
-    }
-    if (player.health > 15) {
-        return 'critical';
-    }
-    return 'wounded';
-};
-
+export default function healthLine(player) {
+  if (player.health > 50) {
+    return 'healthy';
+  } if (player.health > 15) {
+    return 'critical';
+  } return 'wounded';
+}
